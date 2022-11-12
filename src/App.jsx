1b9dcf2c394469ea3a4461d9deeeb0fdf56be158
Florian Mounier <paradoxxx.zero@gmail.com>
2022-11-12 22:03:00 +0100

    Add remove function text

--- src/App.jsx
+++ src/App.jsx
@@ -482,14 +482,31 @@ export function App() {
         <pre className="errors" style={{ color: theme.error }}>
           {errors.map(e => e.message).join(', ')}
         </pre>
-        <input
-          type="text"
-          value={functionsText}
-          style={{
-            color: errors.length ? theme.error : theme.foreground,
-          }}
-          onChange={e => handleFunctions(e.target.value)}
-        />
+        <div className="functionsText">
+          <input
+            type="text"
+            value={functionsText}
+            style={{
+              color: errors.length ? theme.error : theme.foreground,
+            }}
+            onChange={e => handleFunctions(e.target.value)}
+          />
+          <button onClick={() => setFunctionsText('')}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+              style={{ color: theme.foreground }}
+            >
+              <path
+                fill="currentColor"
+                d="M17.414 6.586a2 2 0 0 0-2.828 0L12 9.172L9.414 6.586a2 2 0 1 0-2.828 2.828L9.171 12l-2.585 2.586a2 2 0 1 0 2.828 2.828L12 14.828l2.586 2.586c.39.391.902.586 1.414.586s1.024-.195 1.414-.586a2 2 0 0 0 0-2.828L14.829 12l2.585-2.586a2 2 0 0 0 0-2.828z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
       {settingsOpen ? (
         <div className="modal settings">
